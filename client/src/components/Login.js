275f069e630b13b9b2d6d2c8d64e1bb2a69dad52
Ewa Czech <ewa.czech@hotmail.com>
2020-05-02 20:55:12 +0200

    add mvp

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,59 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+const Login = (props) => {
+	// make a post request to retrieve a token from the api
+	// when you have handled the token, navigate to the BubblePage route
+
+	const [credentials, setCredentials] = useState({});
+
+	const handleInputChange = (e) => {
+		setCredentials({
+			...credentials,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const login = (e) => {
+		e.preventDefault();
+		axiosWithAuth()
+			.post('http://localhost:5000/api/login', credentials)
+			.then((res) => {
+				console.log(credentials);
+				localStorage.setItem('token', res.data.payload);
+				console.log(localStorage.getItem('token'));
+				props.history.push('/colors');
+			})
+			.catch((err) => console.log(err));
+	};
+
+	return (
+		<section>
+			<div>
+				<h1>Welcome to the Bubble App!</h1>
+			</div>
+			<div>
+				<form onSubmit={login}>
+					<input
+						type="text"
+						name="username"
+						value={credentials.username}
+						onChange={handleInputChange}
+						placeholder="Username"
+					/>
+					<input
+						type="password"
+						name="password"
+						value={credentials.password}
+						onChange={handleInputChange}
+						placeholder="Password"
+					/>
+					<button>Log In</button>
+				</form>
+			</div>
+		</section>
+	);
 };
 
 export default Login;
